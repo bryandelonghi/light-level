d9264c42d72b62d9a9744384bd4992d0d7d339d6
bryandelonghi <113536458+bryandelonghi@users.noreply.github.com>
2023-01-09 15:37:16 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,19 @@
 basic.forever(function () {
-    if (input.lightLevel() < 100) {
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
-    } else {
+    if (input.lightLevel() > 100) {
         basic.showLeds(`
             . . . . .
             . . . . .
             . . # . .
             . . . . .
             . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
             `)
     }
 })
